23aaf4bc5de57ebb1165401b1a3bbc593d5b986b
fuchsfoxi <165415707+fuchsfoxi@users.noreply.github.com>
2026-08-24 15:58:27 -0500

    table_productos
    creacion de la tabla productos

--- database/migrations/2026_08_24_014142_create_productos_table.php
+++ database/migrations/2026_08_24_014142_create_productos_table.php
@@ -12,7 +12,12 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('productos', function (Blueprint $table) {
-            $table->id();
+            $table->id('productos_id');
+            $table->string('nombre_producto', 50);
+            $table->date('temporada_fecha')->nullable();
+            $table->boolean('activo')->default(true);
+            $table->foreignId('categorias_id')->constrained('categorias', 'categorias_id');
+            $table->foreignId('unidades_medidas_id')->constrained('unidades_medidas', 'unidades_medidas_id');
             $table->timestamps();
         });
     }
