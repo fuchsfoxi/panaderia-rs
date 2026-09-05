77d1d37a75ec1630a73bde60f4bef96dce8bcfac
fuchsfoxi <165415707+fuchsfoxi@users.noreply.github.com>
2026-09-04 22:01:43 -0500

    tablas roles y turnos

--- database/migrations/2026_08_28_211720_create_turnos_table.php
+++ database/migrations/2026_08_28_211720_create_turnos_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('turnos', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('nombre_turnos', 100);
         });
     }
 
