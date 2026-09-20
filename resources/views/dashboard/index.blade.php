3715a8112bb3e85b166d3afa39727e3d82b61227
fuchsfoxi <165415707+fuchsfoxi@users.noreply.github.com>
2026-09-20 12:27:15 -0500

    seccion produccion
    creacion de la seccion produccion

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -4,6 +4,7 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Dashboard - Panificadora Amazónica</title>
+@vite(['resources/css/dashboard.css', 'resources/js/dashboard.js'])
 </head>
 <body>
 
