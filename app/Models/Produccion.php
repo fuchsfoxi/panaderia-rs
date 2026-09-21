b7788bc0d42b5b9d65a72c0add3e9e22ce748b8c
fuchsfoxi <165415707+fuchsfoxi@users.noreply.github.com>
2026-09-21 16:17:26 -0500

    model/produccion

--- app/Models/Produccion.php
+++ app/Models/Produccion.php
@@ -7,4 +7,21 @@ use Illuminate\Database\Eloquent\Model;
 class Produccion extends Model
 {
     //
+    protected $table = 'produccion';
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'fecha',
+        'registro_por_usuario_id'
+    ];
+
+    public function usuario()
+    {
+        return $this->belongsTo(Usuario::class, 'registrado_por_usuario_id');
+    }
+
+
+
+
 }
