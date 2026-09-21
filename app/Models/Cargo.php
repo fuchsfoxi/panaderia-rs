90276fea0fd85e408d565eb5823c5b0ac4679ba5
fuchsfoxi <165415707+fuchsfoxi@users.noreply.github.com>
2026-09-21 15:10:05 -0500

    models/cargos
    se realizo el contenido del modelo cargos

--- app/Models/Cargo.php
+++ app/Models/Cargo.php
@@ -6,6 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Cargo extends Model
 {
-    //
-    protected $table = 'cargos';
+protected $table = 'cargos';
+
+// esta tabla no tiene created_at/updated_at en la migración, así que le avisamos a Eloquent
+public $timestamps = false;
+
+protected $fillable = [
+'nombre_cargos'
+    ];
+
+// hasMany: un cargo puede tener VARIOS empleados con ese mismo cargo (1 a muchos)
+public function empleados()
+    {
+return $this->hasMany(Empleado::class, 'cargo_id');
+    }
+
 }
