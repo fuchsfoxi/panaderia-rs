626ba3428176ab515e3217a5d1c423cbb209c213
fuchsfoxi <165415707+fuchsfoxi@users.noreply.github.com>
2026-09-21 15:45:50 -0500

    modelo/rol
     creacion del modelo rol

--- app/Models/Rol.php
+++ app/Models/Rol.php
@@ -6,6 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class Rol extends Model
 {
-    //
-    protected $table = 'roles';
+protected $table = 'roles';
+
+public $timestamps = false;
+
+protected $fillable = [
+'nombre_roles'
+    ];
+
+//hasMany se usa cuando UN registro tuyo puede estar relacionado con
+// VARIOS registros de otra tabla.
+
+//Es la versión "multiplicada" de hasOne — recuerda:
+
+//hasOne → "yo tengo uno de esos" (relación 1 a 1)
+//hasMany → "yo tengo varios de esos" (relación 1 a muchos)
+public function usuarios()
+{
+return $this->hasMany(Usuario::class, 'rol_id');
+}
+
 }
