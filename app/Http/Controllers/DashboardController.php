c4915b02679d3047c18dc55944654a42a631df94
fuchsfoxi <165415707+fuchsfoxi@users.noreply.github.com>
2026-09-25 14:11:23 -0500

    controller/historial.blade.php
    se realizo el css, js y el htlml de la pagina historial, tambien se creo el controlador

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -9,7 +9,8 @@ class DashboardController extends Controller
 {
     //
 
-    public function index(){
-        return View('dashboard.index');
-    }
+    public function index()
+        {
+            return view('dashboard.index');
+        }
 }
---
+++ resources/views/history_pedido/index.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> pedido historial </title>
+</head>
+<body>
+    
+        <div class="tittle "></div>
+</body>
+</html>
